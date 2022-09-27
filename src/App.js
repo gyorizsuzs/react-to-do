203bcc72e9e24b814982971f4812b7f332324325
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-09-27 23:00:29 +0200

    creating "complete" status for to do items crUd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo }) {
-  return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
 }
 
 function TodoForm({ addTodo }) {
@@ -29,9 +39,9 @@ function TodoForm({ addTodo }) {
 
 function App() {
   const [todos, setTodos] = useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" },
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet friend for lunch", isCompleted: false },
+    { text: "Build really cool todo app", isCompleted: false },
   ]);
 
   const addTodo = (text) => {
@@ -39,11 +49,22 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
